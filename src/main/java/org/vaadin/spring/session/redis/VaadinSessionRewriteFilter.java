5f02e7df74b3e28633b4a8ad27925b71c7868e55
Alejandro Duarte <alejandro@vaadin.com>
2016-11-10 17:38:45 +0200

    Added Javadoc

--- src/main/java/org/vaadin/spring/session/redis/VaadinSessionRewriteFilter.java
+++ src/main/java/org/vaadin/spring/session/redis/VaadinSessionRewriteFilter.java
@@ -7,6 +7,14 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
+ * Spring Session with Redis doesn't work out-of-the-box with Vaadin. The issue is that the objects in the session are sent to Redis
+ * before the VaadinSession instance has been fully configured which causes an old version of the instance to be persisted in Redis.
+ * The problem resides in the RedisOperationsSessionRepository class which unfortunatelly doesn't have enough extension options to
+ * fix the problem with inheritance for example.
+ * <p>
+ * This Servlet Filter kicks in at the end of a request to "rewrite" the VaadinSession instance into the HttpSession in order to
+ * persist the updated VaadinSession instance in Redis.
+ *
  * @author Alejandro Duarte.
  */
 @WebFilter
